76fabb19955cb0ea6ffc4b4b4b905b58799bdd1d
Evgeniy Kidenko <evgeniykidenko@Samsung.local>
2025-01-28 01:12:38 +0200

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<title>Laravel</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>
+        The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
+        autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
+        even exceed human intelligence and capabilities on tasks such as complex decision-making, reasoning and
+        learning, sophisticated analytics and pattern recognition, visual acuity, speech recognition and language
+        translation. They said “smart” systems in communities, in vehicles, in buildings and utilities, on farms and in
+        business processes will save time, money and lives and offer opportunities for individuals to enjoy a
+        more-customized future.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My Second Post</a></h1>
+    <p>
+        The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
+        autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
+        even exceed human intelligence and capabilities on tasks such as complex decision-making, reasoning and
+        learning, sophisticated analytics and pattern recognition, visual acuity, speech recognition and language
+        translation. They said “smart” systems in communities, in vehicles, in buildings and utilities, on farms and in
+        business processes will save time, money and lives and offer opportunities for individuals to enjoy a
+        more-customized future.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My Third Post</a></h1>
+    <p>
+        The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
+        autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
+        even exceed human intelligence and capabilities on tasks such as complex decision-making, reasoning and
+        learning, sophisticated analytics and pattern recognition, visual acuity, speech recognition and language
+        translation. They said “smart” systems in communities, in vehicles, in buildings and utilities, on farms and in
+        business processes will save time, money and lives and offer opportunities for individuals to enjoy a
+        more-customized future.
+    </p>
+</article>
+</body>
+
