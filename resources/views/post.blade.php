d81203b8c787d92868203a680f516ed2534072c5
evgeniykidenko <evgeniykidenko@gmail.com>
2025-01-28 23:12:43 +0200

    day2

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,19 +2,9 @@
 <title>Laravel</title>
 <link rel="stylesheet" href="/app.css">
 <body>
-<article>
-    <h1><a href="/post">My First Post</a></h1>
-    <p>
-        The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
-        autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
-        even exceed human intelligence and capabilities on tasks such as complex decision-making, reasoning and
-        learning, sophisticated analytics and pattern recognition, visual acuity, speech recognition and language
-        translation. They said “smart” systems in communities, in vehicles, in buildings and utilities, on farms and in
-        business processes will save time, money and lives and offer opportunities for individuals to enjoy a
-        more-customized future.
-    </p>
-</article>
-
+    <article>
+       <?= $post; ?>
+    </article>
     <a href="/">Go back</a>
 </body>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="/app.css">
 <body>
 <article>
-    <h1><a href="/post">My First Post</a></h1>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
     <p>
         The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
         autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
@@ -16,7 +16,7 @@
 </article>
 
 <article>
-    <h1><a href="/post">My Second Post</a></h1>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
     <p>
         The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
         autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
@@ -29,7 +29,7 @@
 </article>
 
 <article>
-    <h1><a href="/post">My Third Post</a></h1>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
     <p>
         The experts predicted networked artificial intelligence will amplify human effectiveness but also threaten human
         autonomy, agency and capabilities. They spoke of the wide-ranging possibilities; that computers might match or
