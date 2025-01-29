a4c0f9324c9232bb00e9fd0a0d24aa53a95a2d39
evgeniykidenko <evgeniykidenko@gmail.com>
2025-01-29 19:14:25 +0200

    day3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use function PHPSTORM_META\map;
 
 Route::get('/', function () {
     return view('posts', [
