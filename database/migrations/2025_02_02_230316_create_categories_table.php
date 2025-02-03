f6794242bea252f9e61cc59a500d17c9124cc0f9
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-03 23:18:23 +0200

    - configured database seeding; - added associating a blog post with the author;

--- database/migrations/2025_02_02_230316_create_categories_table.php
+++ database/migrations/2025_02_02_230316_create_categories_table.php
@@ -6,15 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,53 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+                'name' => 'Personal',
+                'slug' => 'personal'
+            ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>When I was young</p>',
+            'body' => '<p>The rooms were so much colder then, My father was a soldier then, And times were very hard, When I was young, When I was young</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>When I was young</p>',
+            'body' => '<p>The rooms were so much colder then, My father was a soldier then, And times were very hard, When I was young, When I was young</p>'
         ]);
     }
 }
