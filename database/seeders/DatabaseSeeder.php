925bc2cd291fd369039f50f0e6bb3e75824ae240
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-04 12:11:56 +0200

    - added factories for quick seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,53 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-                'name' => 'Personal',
-                'slug' => 'personal'
-            ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
+        Post::factory(5)->create([
             'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>When I was young</p>',
-            'body' => '<p>The rooms were so much colder then, My father was a soldier then, And times were very hard, When I was young, When I was young</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>When I was young</p>',
-            'body' => '<p>The rooms were so much colder then, My father was a soldier then, And times were very hard, When I was young, When I was young</p>'
         ]);
     }
 }
