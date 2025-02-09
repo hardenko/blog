b780ad9b39853274cb1b0807691f82af31aa2a31
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-09 23:36:54 +0200

    - added messages for users when the validation checker fails. - implemented fetching old input data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,8 +15,13 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -30,8 +35,13 @@
                             type="text"
                             name="username"
                             id="username"
+                            value="{{ old('username') }}"
                             required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -45,8 +55,13 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -62,6 +77,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
