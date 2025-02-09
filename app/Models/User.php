30ba67d643b78939b99e873ac0e6704dbc1104cd
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-09 23:14:51 +0200

    - added bcrypt for user's passwords; - leveraged eloquent mutators to ensure that passwords are always hashed before "bcrypt"ing.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -41,6 +42,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
     }
 
     public function posts()
