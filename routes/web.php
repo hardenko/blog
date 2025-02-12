aa43880e7979eb7d65da6ed892f64c6a29e4ed5f
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-12 21:04:22 +0200

    - connected mailchimp using API - made some initial API calls

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us7'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('0ac744ed56', [
+        'email_address' => 'yevhenkidenko@gmail.com',
+        'status' => 'subscribed'
+    ]);
+
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
