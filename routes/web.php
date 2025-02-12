49df11c849a110a3bddadf7c4c589caef527e40e
evgeniykidenko <evgeniykidenko@gmail.com>
2025-02-12 21:20:09 +0200

    - updated newsletter form

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+    request()->validate(['email' => 'required|email']);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -14,12 +16,19 @@ Route::get('ping', function () {
         'server' => 'us7'
     ]);
 
-    $response = $mailchimp->lists->addListMember('0ac744ed56', [
-        'email_address' => 'yevhenkidenko@gmail.com',
-        'status' => 'subscribed'
-    ]);
+    try {
+        $response = $mailchimp->lists->addListMember('0ac744ed56', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
 
-    dd($response);
+    return redirect('/')
+        ->with('success', 'You are now subscribed.');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
